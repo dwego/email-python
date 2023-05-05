90fe8bfe8fa67e3706429d7448499ea42625965c
dwego <97767711+dwego@users.noreply.github.com>
2023-05-04 23:07:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,46 +3,47 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import random
-# alt = numero aleatorio de 5 digitos para verificação
 
+# alt = random 5-digit verification number
 alt = random.randint(10000,100000)
 alt = str(alt)
 
-mail = input('Digite o seu email para verificarmos: ')
+mail = input('Enter your email for verification: ')
 
 host = "smtp.gmail.com"
 port = "587"
-# Email e senha
-login = ""
-senha = ""
 
-# Conectando ao servidor
+# Email and password
+login = ""
+password = ""
+
+# Connecting to server
 server = smtplib.SMTP(host,port)
 
 server.ehlo()
 server.starttls()
 
-server.login(login,senha)
-# Montando email
+server.login(login,password)
 
-body = f"Seu codigo de verificação é <b>{alt}</b>"
+# Building email
+body = f"Your verification code is <b>{alt}</b>"
 email_msg = MIMEMultipart()
-# Enviando email
 
+# Sending email
 email_msg['From'] = login
 email_msg['To'] = mail
-email_msg['Subject'] = "Codigo de verificação"
+email_msg['Subject'] = "Verification Code"
 email_msg.attach(MIMEText(body,'html'))
 
 server.sendmail(email_msg['From'], email_msg['To'], email_msg.as_string())
 server.quit()
-# Verificando codigo
 
-print('Digite o cogido de verificação enviado para {}'.format(mail))
-codigo = input('')
-codigo = codigo.strip()
+# Verifying code
+print('Enter the verification code sent to {}'.format(mail))
+code = input('')
+code = code.strip()
 
-if codigo == alt:
-    print('Correto')
+if code == alt:
+    print('Correct')
 else:
-    print('Incorreto')
+    print('Incorrect')
